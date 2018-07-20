da06c529da285f13f2743c2251c1fa3616bfa88e
mourineboyani <mourineboyani@gmail.com>
2018-07-20 19:27:15 +0300

    modified the index. html and the css

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,72 @@
-$(document).ready(function() {
+//business logic
+var player1 = "";
+var player2 = "";
 
-  $("button#start").click(function(event){
+var throwdice = function () {
+  return Math.floor(6 * Math.random()) + 1;
+}
+
+function Player(turn) {
+  this.roll = 0;
+  this.tempscore = 0;
+  this.totalscore = 0;
+  this.turn = turn;
+  this.playerName;
+}
+
+// checking for 1
+Player.prototype.rollone = function () {
+  if (this.roll === 1) {
+    this.tempscore = 0;
+    alert("Sorry " + this.playerName + ", you rolled a 1! Your turn is over!")
+    // this.changeturn();
+  } else {
+    this.tempscore += this.roll;
+  }
+}
+
+// hold
+Player.prototype.hold = function () {
+  this.totalscore += this.tempscore;
+  this.tempscore = 0;
+  // this.changeturn();
+  alert(this.playerName + ", your turn is over, pass the mouse!");
+}
+
+// // changing turn
+// Player.prototype.changeturn = function () {
+//   if (this.roll ===1) {
+//     this.turn = false;
+//   } else {
+//     this.turn = true;
+//   }
+// }
+// check for 100
+Player.prototype.winnerCheck = function () {
+  if (this.totalscore >= 100) {
+    alert(this.playerName + " You are the winner!");
+  }
+}
+
+Player.prototype.newGame = function () {
+  //debugger;
+  this.roll = 0;
+  this.tempscore = 0;
+  this.totalscore = 0;
+  this.playerName = "";
+}
+
+var clearValues = function () {
+  $(".player1Name").val("");
+  $(".player2Name").val("");
+}
+
+// User Interface
+$(document).ready(function () {
+
+  $("button#start").click(function (event) {
     player1 = new Player(true);
-    player2 =  new Player(false);
+    player2 = new Player(false);
     $(".player-console").show();
     $(".start-menu").hide();
 
@@ -12,11 +76,11 @@ $(document).ready(function() {
     var player2Name = $(".player2Name").val();
     $("#player2Name").text(player2Name);
 
-    player1.playerName=player1Name;
-    player2.playerName=player2Name;
+    player1.playerName = player1Name;
+    player2.playerName = player2Name;
 
   });
-  $("button#new-game").click(function(event){
+  $("button#new-game").click(function (event) {
     $(".player-console").hide();
     clearValues();
     player1.newGame();
@@ -31,21 +95,21 @@ $(document).ready(function() {
     $(".start-menu").show();
   });
 
-  $("button#player1-roll").click(function(event){
+  $("button#player1-roll").click(function (event) {
     player1.roll = throwdice();
     $("#die-roll-1").text(player1.roll);
     player1.rollone();
     $("#round-total-1").text(player1.tempscore);
   });
 
-  $("button#player2-roll").click(function(event){
+  $("button#player2-roll").click(function (event) {
     player2.roll = throwdice();
     $("#die-roll-2").text(player2.roll);
     player2.rollone();
     $("#round-total-2").text(player2.tempscore);
   });
 
-  $("button#player1-hold").click(function(event){
+  $("button#player1-hold").click(function (event) {
     player1.hold();
     $("#total-score-1").text(player1.totalscore);
     $("#round-total-1").empty();
@@ -53,7 +117,7 @@ $(document).ready(function() {
     player1.winnerCheck();
   });
 
-  $("button#player2-hold").click(function(event){
+  $("button#player2-hold").click(function (event) {
     player2.hold();
     $("#total-score-2").text(player2.totalscore);
     $("#round-total-2").empty();
@@ -62,3 +126,14 @@ $(document).ready(function() {
   });
 
 });
+
+
+
+
+
+$().ready(function(){
+  $("#start").click(function(){
+    $("#pn").hide(400);
+    $("#kj").show(1000);
+  })
+})
